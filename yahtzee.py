a9f4a480414b38ab62c07dc745172433be80b6c8
Andy Hua <user.andy.hua@gmail.com>
2022-09-17 15:58:44 -0700

    initial commit

---
+++ yahtzee.py
@@ -0,0 +1,23 @@
+def main():
+    roll()
+
+
+def roll():
+    pass
+
+
+def reroll():
+    pass
+
+
+def draw_dice(list):
+    "⚀⚁⚂⚃⚄⚅"
+    pass
+
+
+def score_table():
+    pass
+
+
+if __name__ == "__main__":
+    main()
