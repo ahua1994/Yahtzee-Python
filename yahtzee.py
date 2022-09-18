9dd207efbbbbe40a23edceefbd411c8db88fd67f
Andy Hua <user.andy.hua@gmail.com>
2022-09-18 00:14:25 -0700

    added score table and dice roll

--- yahtzee.py
+++ yahtzee.py
@@ -1,9 +1,23 @@
+from random import random
+from tabulate import tabulate
+import math
+
+finished = []
+
+
 def main():
-    roll()
+    table = [["Category", "Ones", "Twos", "Threes", "Fours", "Fives", "Sixes", "3 of a\nKind",
+                          "4 of a\nKind", "Full\nHouse", "Small\nStraight", "Large\nStraight", "Yahtzee", "Chance"], ["Current\nScore", *[0]*13], ["Score\nTo Add", *[0]*13]]
+    # score_table(table)
+    dice = roll()
+    print(dice, draw_dice(dice))
 
 
 def roll():
-    pass
+    dice = []
+    for _ in range(5):
+        dice.append(math.ceil(random() * 6))
+    return dice
 
 
 def reroll():
@@ -11,12 +25,18 @@ def reroll():
 
 
 def draw_dice(list):
-    "⚀⚁⚂⚃⚄⚅"
+    d6 = ["⚀", "⚁", "⚂", "⚃", "⚄", "⚅"]
+    return [d6[i-1] for i in list]
+
+
+def add_score(table):
     pass
 
 
-def score_table():
-    pass
+def score_table(table):
+    # table for all 3 rows, table[:2] for first 2 rows
+
+    print(tabulate(table[:2], tablefmt="grid"))
 
 
 if __name__ == "__main__":
